5d9eaf272f79ac2d8a2c2995e6bc61b276538f1a
Brian Green <briangreenery@gmail.com>
2015-12-08 00:53:54 -0800

    Always operate on raw bytes

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,4 +1,4 @@
-fn digit_value(digit: u8) -> u8 {
+fn hex_digit_value(digit: u8) -> u8 {
     match digit {
         48...57 => digit - 48,
         65...90 => digit - 65 + 10,
@@ -7,31 +7,32 @@ fn digit_value(digit: u8) -> u8 {
     }
 }
 
-fn hex_value(digits: &[u8]) -> u8 {
-    16 * digit_value(digits[0]) + digit_value(digits[1])
+fn hex_to_bytes(hex: &str) -> Vec<u8> {
+    hex.as_bytes()
+       .chunks(2)
+       .map(|digits: &[u8]| 16 * hex_digit_value(digits[0]) + hex_digit_value(digits[1]))
+       .collect()
 }
 
-fn get_value(accum: u32, index: u32) -> usize {
+fn base64_value(accum: u32, index: u32) -> usize {
     ((accum >> (18 - 6 * index)) % 64) as usize
 }
 
-fn hex_to_base64(hex: &str) -> String {
+fn bytes_to_base64(bytes: &Vec<u8>) -> String {
     let table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".as_bytes();
 
     let mut result: Vec<u8> = Vec::new();
     let mut accum: u32 = 0;
     let mut count: u8 = 0;
 
-    for b in hex.as_bytes().chunks(2) {
-        let value = hex_value(b) as u32;
-
+    for byte in bytes.iter() {
         accum <<= 8;
-        accum |= value;
+        accum |= *byte as u32;
 
         count += 1;
         if count == 3 {
             for i in 0..4 {
-                result.push(table[get_value(accum, i)]);
+                result.push(table[base64_value(accum, i)]);
             }
 
             accum = 0;
@@ -42,16 +43,16 @@ fn hex_to_base64(hex: &str) -> String {
     match count {
         1 => {
             accum <<= 16;
-            result.push(table[get_value(accum, 0)]);
-            result.push(table[get_value(accum, 1)]);
+            result.push(table[base64_value(accum, 0)]);
+            result.push(table[base64_value(accum, 1)]);
             result.push(61);
             result.push(61);
         }
         2 => {
             accum <<= 8;
-            result.push(table[get_value(accum, 0)]);
-            result.push(table[get_value(accum, 1)]);
-            result.push(table[get_value(accum, 2)]);
+            result.push(table[base64_value(accum, 0)]);
+            result.push(table[base64_value(accum, 1)]);
+            result.push(table[base64_value(accum, 2)]);
             result.push(61);
         }
         _ => {}
@@ -60,8 +61,14 @@ fn hex_to_base64(hex: &str) -> String {
     return String::from_utf8(result).unwrap();
 }
 
+fn hex_to_base64(hex: &str) -> String {
+    bytes_to_base64(&hex_to_bytes(hex))
+}
+
 fn main() {
-    println!("{}", hex_to_base64("49276d"));
+    println!("{}",
+             hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f6973\
+                            6f6e6f7573206d757368726f6f6d"));
 }
 
 #[test]
