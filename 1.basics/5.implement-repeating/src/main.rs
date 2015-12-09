2ef9e0651274ccb85a35a52fda1959d14e942ad6
Brian Green <briangreenery@gmail.com>
2015-12-09 00:59:01 -0800

    rustfmt

--- 1.basics/5.implement-repeating/src/main.rs
+++ 1.basics/5.implement-repeating/src/main.rs
@@ -10,7 +10,7 @@ fn hex_from_num(num: u8) -> u8 {
 struct ReapeatingXOR<R> {
     inner: R,
     index: usize,
-    key: Vec<u8>
+    key: Vec<u8>,
 }
 
 impl<R: std::io::Read> ReapeatingXOR<R> {
@@ -18,24 +18,24 @@ impl<R: std::io::Read> ReapeatingXOR<R> {
         ReapeatingXOR {
             inner: inner,
             index: 0,
-            key: key.as_bytes().to_vec()
+            key: key.as_bytes().to_vec(),
         }
     }
 }
 
 impl<R: std::io::Read> Iterator for ReapeatingXOR<R> {
     type Item = std::io::Result<u8>;
-    
+
     fn next(&mut self) -> Option<std::io::Result<u8>> {
         let mut buf = [0];
-        
+
         match self.inner.read(&mut buf) {
             Ok(0) => None,
             Ok(..) => {
                 let encrypted = self.key[self.index] ^ buf[0];
                 self.index = (self.index + 1) % self.key.len();
                 Some(Ok(encrypted))
-            },
+            }
             Err(e) => Some(Err(e)),
         }
     }
@@ -44,9 +44,9 @@ impl<R: std::io::Read> Iterator for ReapeatingXOR<R> {
 fn main() {
     let stdin = std::io::stdin();
     let mut stdout = std::io::stdout();
-    
+
     let xor = ReapeatingXOR::new(stdin, "ICE");
-    
+
     for byte in xor {
         let value = byte.unwrap();
         let hex = [hex_from_num(value / 16), hex_from_num(value % 16)];
