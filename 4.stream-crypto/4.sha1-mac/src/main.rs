9f558742e36f6adee93703730f529014d57e927c
Brian Green <briangreenery@gmail.com>
2015-12-30 22:22:47 -0800

    better sha1

--- 4.stream-crypto/4.sha1-mac/src/main.rs
+++ 4.stream-crypto/4.sha1-mac/src/main.rs
@@ -5,6 +5,6 @@ extern crate crypto;
 extern crate rand;
 
 fn main() {
-    let hash = pals::sha1::hash(b"Hodor");
+    let hash = pals::sha1::hash(b"hodor");
     println!("{}", pals::hex::encode(&hash));
 }
