2a24a27910c13096b59dc62aae4d4c06e8eb9f32
Brian Green <briangreenery@gmail.com>
2016-01-05 21:41:51 -0800

    Get started with hyper

--- 4.stream-crypto/7.slow-hmac/src/main.rs
+++ 4.stream-crypto/7.slow-hmac/src/main.rs
@@ -3,7 +3,17 @@ mod pals;
 
 extern crate crypto;
 extern crate rand;
+extern crate hyper;
+
+use hyper::Server;
+use hyper::server::Request;
+use hyper::server::Response;
+
+fn hello(_: Request, res: Response) {
+    res.send(b"Hello World!").unwrap();
+}
 
 fn main() {
-    println!("{}", pals::hex::encode(&pals::sha1::hmac(b"key", b"hodor")));
+    // println!("{}", pals::hex::encode(&pals::sha1::hmac(b"key", b"hodor")));
+    Server::http("127.0.0.1:3000").unwrap().handle(hello).unwrap();
 }
