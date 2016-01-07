e57ef91705ab9845c24d27622b09dccc835b4d00
Brian Green <briangreenery@gmail.com>
2016-01-06 18:36:39 -0800

    Complete 4.7

--- 4.stream-crypto/7.slow-hmac/src/main.rs
+++ 4.stream-crypto/7.slow-hmac/src/main.rs
@@ -5,6 +5,7 @@ extern crate crypto;
 extern crate rand;
 extern crate hyper;
 extern crate url;
+extern crate time;
 
 use url::{Url, UrlParser};
 
@@ -12,6 +13,10 @@ use hyper::Server;
 use hyper::server::Request;
 use hyper::server::Response;
 use hyper::uri::RequestUri::AbsolutePath;
+
+use hyper::Client;
+
+use std::thread;
 
 fn get_parts(path: &str) -> (String, String) {
     let base = Url::parse("http://localhost:9000").unwrap();
@@ -31,16 +36,15 @@ fn get_parts(path: &str) -> (String, String) {
     (file, signature)
 }
 
-fn slow_equal(a: &[u8], b: &[u8]) -> bool {
+fn insecure_compare(a: &[u8], b: &[u8]) -> bool {
     let min = std::cmp::min(a.len(), b.len());
-    let time = std::time::Duration::from_millis(50);
 
     for i in 0..min {
-        std::thread::sleep(time);
-
         if a[i] != b[i] {
             return false;
         }
+
+        std::thread::sleep_ms(50);
     }
 
     return true;
@@ -55,7 +59,7 @@ fn hello(req: Request, mut res: Response) {
         let computed = pals::sha1::hmac(key, file.as_bytes());
         let provided = pals::hex::decode(&signature).unwrap();
 
-        if slow_equal(&computed, &provided) {
+        if insecure_compare(&computed, &provided) {
             *res.status_mut() = hyper::status::StatusCode::Ok;
             res.send(b"ok").unwrap();
         } else {
@@ -67,6 +71,71 @@ fn hello(req: Request, mut res: Response) {
     }
 }
 
+fn try(file: &str, signature: &[u8; 20]) -> bool {
+    let client = Client::new();
+
+    let mut url = String::new();
+    url.push_str("http://127.0.0.1:9000/test?file=");
+    url.push_str(file);
+    url.push_str("&signature=");
+    url.push_str(&pals::hex::encode(signature));
+
+    let res = client.get(&*url)
+                    .header(hyper::header::Connection::close())
+                    .send()
+                    .unwrap();
+
+    res.status == hyper::Ok
+}
+
+fn now() -> i64 {
+    let timespec = time::now_utc().to_timespec();
+    (1000000000 * timespec.sec) + (timespec.nsec as i64)
+}
+
 fn main() {
-    Server::http("127.0.0.1:9000").unwrap().handle(hello).unwrap();
+    let computed = pals::sha1::hmac(b"hodor", b"foo");
+
+    let handle = thread::spawn(|| {
+        Server::http("127.0.0.1:9000").unwrap().handle(hello).unwrap();
+    });
+
+    let mut buffer = [0; 20];
+
+    for index in 0..buffer.len() {
+        println!("computed: {}", &pals::hex::encode(&computed));
+        println!("guessed:  {}", &pals::hex::encode(&buffer));
+
+        let mut best_time = 0;
+        let mut best_byte = 0;
+
+        for byte in 0..256 {
+            buffer[index] = byte as u8;
+
+            let start = now();
+
+            for _ in 0..10 {
+                try("foo", &buffer);
+            }
+
+            let sum = now() - start;
+
+            if sum > best_time {
+                best_time = sum;
+                best_byte = byte as u8;
+
+                println!("best is {} at {}",
+                         &pals::hex::encode(&[best_byte]),
+                         (best_time as f64) / 1000000.0);
+            }
+        }
+
+        buffer[index] = best_byte;
+    }
+
+    if try("foo", &buffer) {
+        println!("valid signature is {}", &pals::hex::encode(&buffer));
+    }
+
+    handle.join().unwrap();
 }
