340529629774f1846b888313391fe8a117e273c4
Brian Green <briangreenery@gmail.com>
2016-01-27 16:03:24 -0800

    Complete 6.3

--- 6.rsa-and-dsa/3.dsa-key-recovery/src/main.rs
+++ 6.rsa-and-dsa/3.dsa-key-recovery/src/main.rs
@@ -6,6 +6,16 @@ extern crate rand;
 
 use pals::BigNum;
 
+fn dsa_private_key(q: &BigNum, rinv: &BigNum, s: &BigNum, h: &BigNum, k: &BigNum) -> BigNum {
+    let mut sk = s.mul(&k);
+
+    while &sk < h {
+        sk = sk.add(&q);
+    }
+
+    sk.sub(&h).mul(&rinv).div(&q).1
+}
+
 fn main() {
     let p = BigNum::from_bytes(&pals::hex::decode("800000000000000089e1855218a0e7dac38136ffafa72\
                                                    eda7859f2171e25e65eac698c1702578b07dc2a1076da\
@@ -26,17 +36,34 @@ fn main() {
                                                    bdd41a7054ec89f756ba9fc95302291")
                                     .unwrap());
 
-    let key = pals::DSA::new(p, q, g);
-
     let text = "For those that envy a MC it can be hazardous to your health\nSo be friendly, a \
                 matter of life and death, just like a etch-a-sketch\n";
 
-    let signature = key.sign(text.as_bytes());
-    println!("signature is:");
-    println!("r: {}", signature.0.to_decimal());
-    println!("s: {}", signature.1.to_decimal());
-    println!("");
+    let r = BigNum::from_decimal("548099063082341131477253921760299949438196259240");
+    let s = BigNum::from_decimal("857042759984254168557880549501802188789837994940");
+    let h = BigNum::from_bytes(&pals::sha1::hash(text.as_bytes()));
 
-    let valid = key.verify(text.as_bytes(), &signature);
-    println!("signature is valid: {}", valid);
+    let y = BigNum::from_bytes(&pals::hex::decode("084ad4719d044495496a3201c8ff484feb45b962e7302\
+                                                   e56a392aee4abab3e4bdebf2955b4736012f21a080840\
+                                                   56b19bcd7fee56048e004e44984e2f411788efdc837a0\
+                                                   d2e5abb7b555039fd243ac01f0fb2ed1dec568280ce67\
+                                                   8e931868d23eb095fde9d3779191b8c0299d6e07bbb28\
+                                                   3e6633451e535c45513b2d33c99ea17")
+                                    .unwrap());
+
+    let rinv = r.modinv(&q);
+
+    for k in 1..65536 {
+        let x = dsa_private_key(&q, &rinv, &s, &h, &BigNum::new(k));
+        let y2 = g.modexp(&x, &p);
+
+        if y == y2 {
+            println!("private key is: {}", pals::hex::encode(&x.to_bytes()));
+            break;
+        }
+
+        if k % 1024 == 0 {
+            println!("k = {}", k);
+        }
+    }
 }
